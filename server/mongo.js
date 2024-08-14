c043678233458ddcb630aa0ba9c10ed62663f351
kunaldeshpande2004 <kunalgirish135@gmail.com>
2024-08-14 23:53:16 +0530

    first-commit

--- server/mongo.js
+++ server/mongo.js
@@ -1,4 +1,4 @@
-const mongoose=require('mongoose')
+/*const mongoose=require('mongoose')
 mongoose.connect('mongodb://localhost:27017/')
 .then(()=>{
     console.log('connected')
@@ -6,6 +6,21 @@ mongoose.connect('mongodb://localhost:27017/')
 .catch(()=>{
     console.log('failed')
 })
+*/
+
+const mongoose = require('mongoose');
+
+const uri = "mongodb+srv://kunalgirish135:EIjP5YYzondpVPaT@cluster0.iarmh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB Atlas via Mongoose!");
+}).catch((err) => {
+    console.error("Error connecting to MongoDB Atlas:", err);
+});
+
 
 const bookingSchema=new mongoose.Schema({
     car: String,
